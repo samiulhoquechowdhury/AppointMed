9acb8dc6e76f167d2488c9dba22cc8e4fc3d2e44
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-28 19:11:27 +0530

    routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctor from "./pages/Doctors";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppointment from "./pages/MyAppointment";
+import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="mx-4 sm:mx-[10%]">
+      {/* components */}
+      <Navbar />
 
-export default App
+      {/* routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctor />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointment />} />
+        <Route path="/appointment/:docId" element={<Appointment />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        "primary": "#5f6FFF"
+      }
+    },
   },
   plugins: [],
 }
