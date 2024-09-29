eae33df61ca9215aab4077284d01c44bd1510a70
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-29 16:33:02 +0530

    add footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,22 +3,21 @@ import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div className=" flex">
-      <div className="flex items-center justify-between">
+    <div className=" md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         {/* ---Left Section---- */}
-        <div className="w-1/3">
-          <img src={assets.logo} alt="" width={60} />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-            fugit voluptate recusandae accusantium quis. Ipsam perspiciatis
-            sunt, esse maxime autem ex dolore eveniet quam ratione culpa est
-            debitis sapiente aliquam?
+        <div className="">
+          <img className="mb-5" src={assets.logo} alt="" width={60} />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
+            AppointMed is your trusted online solution for hassle-free doctor
+            appointments. We connect patients with healthcare professionals,
+            offering a seamless experience for booking consultations.
           </p>
         </div>
         {/* certer section */}
         <div>
-          <p>COMPANY</p>
-          <ul>
+          <p className="text-xl font-medium mb-4 ">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>Home</li>
             <li>About us</li>
             <li>Contact us</li>
@@ -27,12 +26,19 @@ const Footer = () => {
         </div>
         {/* right section */}
         <div>
-          <p>GET IN TOUCH</p>
-          <ul>
+          <p className="text-xl font-medium mb-4">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>+91-9876543210</li>
             <li>developer@sam.com</li>
           </ul>
         </div>
+      </div>
+      {/* copyright section */}
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2024@ AppointMed - All right reserved
+        </p>
       </div>
     </div>
   );
