14fcfa0ae962e8eb180bbae3d52185bb9da33cd6
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2024-10-09 22:44:55 +0530

    database setup

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,20 +1,17 @@
 // config/mongodb.js
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(`${MONGODB_URI}/appointmed`);
-        console.log('Database connected successfully');
-    } catch (err) {
-        console.error('Database connection failed:', err.message);
-        process.exit(1); 
-    }
+  try {
+    await mongoose.connect(`${MONGODB_URI}/appointmed`);
+    console.log("Database connected successfully");
+  } catch (err) {
+    console.error("Database connection failed:", err.message);
+    process.exit(1);
+  }
 };
 
-// Export the connection function
 export default connectDB;
