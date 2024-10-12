46cfeaa9e759d0292271acef61adfb2d867320c7
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2024-10-12 21:35:36 +0530

    admin controller added

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,3 +1,4 @@
+import validator from "validator";
 //api for adding doctors
 const addDoctor = async (req, res) => {
   try {
@@ -13,18 +14,36 @@ const addDoctor = async (req, res) => {
       address,
     } = req.body();
     const imageFile = req.file;
-    // test the data
-    // console.log({
-    //   name,
-    //   email,
-    //   password,
-    //   speciality,
-    //   degree,
-    //   experience,
-    //   about,
-    //   fee,
-    //   address,
-    // });
+    //checking for all data to add doctor
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !fee ||
+      !address
+    ) {
+      return res.json({ success: false, message: "All fields are required" });
+    }
+
+    //validate email format
+
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+    //validating strong password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
   } catch (error) {
     console.error(error.message);
   }
