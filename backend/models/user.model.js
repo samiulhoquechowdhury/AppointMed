15011dd0efa390c9d80f1b1ef85c43a392d082c0
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2024-10-14 21:26:12 +0530

    user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,42 +1,44 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required:true
+      type: String,
+      required: true,
     },
     image: {
-        type: String,
-        required:true
+      type: String,
+      default: "", // add default profile image after converting it ( image to data base64)
     },
-    
+
     address: {
-        type: Object,
-        default:{line1:'', line2:''}
+      type: Object,
+      default: { line1: "", line2: "" },
     },
     gender: {
-        type: String,
-        default:"not selected"
+      type: String,
+      default: "not selected",
     },
     dob: {
-        type: String,
-        default:"not selected"
+      type: String,
+      default: "not selected",
     },
     phone: {
-        type: String,
-        default:"0000000000"
+      type: String,
+      default: "0000000000",
     },
-    
-},{minimize:false})
+  },
+  { minimize: false }
+);
 
-const userModel = mongoose.model.doctor || mongoose.model('user', userSchema) 
-export default userModel
+const userModel = mongoose.model.doctor || mongoose.model("user", userSchema);
+export default userModel;
